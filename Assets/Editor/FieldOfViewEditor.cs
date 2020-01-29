df2b080b149dacc92d865d1d2300456b63d5ccd8
Steve Lyu <steveLyu-Github@outlook.com>
2020-01-29 20:52:13 +0800

    Add project files.

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(FieldOfView))]
+public class FiledOfViewEditor : Editor {
+    
+    void OnSceneGUI() {
+        //绘制视野范围
+        FieldOfView fov = (FieldOfView) target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        //绘制可视角度
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach(Transform visibleTarget in fov.visibleTargets) {
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+}
+
+
+
+
+
---
+++ Assets/FieldOfView.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    //Mesh
+    public float meshResolution;
+    public float edgeResolveIterations;
+    public float edgeDstThreshold;
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
+
+
+    void Start() {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+        
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    //使用LateUpdate来保证mesh在Controller输入之后绘制， 从而避免mesh出现抖动效果
+    void LateUpdate() {
+        DrawFieldOfView();
+    }
+
+    //检测视野中的可见物体
+    IEnumerator FindTargetsWithDelay(float delay) {
+        while (true) {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTarget();
+        }
+    }
+
+    void FindVisibleTarget() {
+        visibleTargets.Clear();
+        //存储所有处在视野距离内的碰撞体
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++) {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            // 如果碰撞体仍然处在视野范围内，则进行下一步的射线检测
+            if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+    //绘制视野效果(绘制一个mesh)
+    private void DrawFieldOfView() {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+
+        for (int i = 0; i <= stepCount; i++) {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            
+            //二分法逼近障碍物的边缘，从而使绘制的mesh更加光滑
+            if (i > 0) {
+                //角度很接近的两条射线可能碰撞到两个不同的物体上，造成mesh绘制错误，需要避免
+                bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded)) {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero) {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero) {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
+            viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++) {
+            //viewPoints 是世界坐标下的点， 需要转换回 localscale
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2) {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    private ViewCastInfo ViewCast(float globalAngle) {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask)) {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }else {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+
+    }
+
+    private EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast) {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        //二分法求近似边缘
+        for (int i = 0; i < edgeResolveIterations; i++) {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+            if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded) {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            } else {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    //返回特定角度的方向向量
+    public Vector3 DirFromAngle(float angleInDegress, bool angleIsGlobal) {
+        //如果不是世界坐标系下的角度，需要加上原本就有的角度
+        if (!angleIsGlobal) {
+            angleInDegress += transform.eulerAngles.y;
+        }
+        //unity 中角度是顺时针增加的，所以使用参数方程表示角度需要交换Sin()和Cos()
+        return new Vector3(Mathf.Sin(angleInDegress * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegress * Mathf.Deg2Rad));
+    }
+    
+    //ViewCastInfo 结构体
+    public struct ViewCastInfo {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle) {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+
+    //EdgeInfo 结构体
+    public struct EdgeInfo {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 _pointA, Vector3 _pointB) {
+            pointA = _pointA;
+            pointB = _pointB;
+        }
+    }
+}
